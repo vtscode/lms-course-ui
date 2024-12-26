379587197df35897acaa168201e3e871deecf284
vtscode <ventuscode@gmail.com>
2024-12-26 16:02:30 +0700

    fix header signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -33,7 +33,7 @@ export default function SignUpPage() {
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] lg:w-[400px]">
           <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
+            <h1 className="text-2xl font-semibold tracking-tight mt-[32px]">
               Create your account
             </h1>
             <p className="text-sm text-muted-foreground">
